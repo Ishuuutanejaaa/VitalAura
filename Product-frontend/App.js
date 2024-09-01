9d0fbc119d9d9cec219141bcca54a633e0954a0e
Harleen Kaur <127092049+Harleen365@users.noreply.github.com>
2024-09-01 09:49:34 +0530

    Create App.js

---
+++ Product-frontend/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import ProductList from './ProductList'; // Import the ProductList component
+
+const App = () => {
+    return (
+        <div>
+            <ProductList />
+        </div>
+    );
+};
+
+export default App;
